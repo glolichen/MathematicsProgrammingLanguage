37f54e8a1721205c8af046ed8e8ac9b84620183d
glolichen <jlche18659@gmail.com>
2023-09-22 21:03:30 -0400

    put everything in namespace

--- parser.hpp
+++ parser.hpp
@@ -11,68 +11,67 @@ grouping: "(" + expression + ")"
 
 */
 
-class Expression {};
+namespace Parser {
+	class Expression {};
 
-class Binary : Expression {
-public:
-	Expression left, right;
-	lexer::Token operation;
-	Binary(Expression left, lexer::Token operation, Expression right) {
-		this->left = left;
-		this->operation = operation;
-		this->right = right;
-	}
-};
+	class Binary : Expression {
+	public:
+		Expression left, right;
+		lexer::Token operation;
+		Binary(Expression left, lexer::Token operation, Expression right) {
+			this->left = left;
+			this->operation = operation;
+			this->right = right;
+		}
+	};
 
-class Assign : Expression {
-public:
-	lexer::Token name;
-	Expression value;
-	Assign(lexer::Token name, Expression value) {
-		this->name = name;
-		this->value = value;
-	}
-};
+	class Assign : Expression {
+	public:
+		lexer::Token name;
+		Expression value;
+		Assign(lexer::Token name, Expression value) {
+			this->name = name;
+			this->value = value;
+		}
+	};
 
-class Group : Expression {
-public:
-	Expression expr;
-	Group(Expression expr) {
-		this->expr = expr;
-	}
-};
+	class Group : Expression {
+	public:
+		Expression expr;
+		Group(Expression expr) {
+			this->expr = expr;
+		}
+	};
 
-class Unary : Expression {
-public:
-	lexer::Token operation;
-	Expression operand;
-	Unary(lexer::Token operation, Expression operand) {
-		this->operation = operation;
-		this->operand = operand;
-	}
-};
+	class Unary : Expression {
+	public:
+		lexer::Token operation;
+		Expression operand;
+		Unary(lexer::Token operation, Expression operand) {
+			this->operation = operation;
+			this->operand = operand;
+		}
+	};
 
-class Assign : Expression {
-public:
-	lexer::Token name;
-	Expression value;
-	Assign(lexer::Token name, Expression value) {
-		this->name = name;
-		this->value = value;
-	}
-};
+	class Assign : Expression {
+	public:
+		lexer::Token name;
+		Expression value;
+		Assign(lexer::Token name, Expression value) {
+			this->name = name;
+			this->value = value;
+		}
+	};
 
-class Assign : Expression {
-public:
-	lexer::Token name;
-	Expression value;
-	Assign(lexer::Token name, Expression value) {
-		this->name = name;
-		this->value = value;
-	}
-};
-
-
-
+	class Assign : Expression {
+	public:
+		lexer::Token name;
+		Expression value;
+		Assign(lexer::Token name, Expression value) {
+			this->name = name;
+			this->value = value;
+		}
+	};
+}
 
 #endif
